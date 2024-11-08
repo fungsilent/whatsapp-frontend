54ad2b71665e1b16f671ccd328ca2c5abe6f7e08
lpxuan0624 <lipeixuan123@gmail.com>
2024-11-08 10:36:45 +0800

    ver 1.0 - created Login2

--- src/app/(public)/login/page.js
+++ src/app/(public)/login/page.js
@@ -1,22 +1,23 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { redirect } from 'next/navigation'
-import useFetch from '#root/hooks/useFetch'
-import { signin } from '#root/api/user'
-import { useAppStore } from '#root/app/store'
+"use client"
+import { useState, useEffect } from "react"
+import { redirect } from "next/navigation"
+import useFetch from "#root/hooks/useFetch"
+import { signin } from "#root/api/user"
+import { useAppStore } from "#root/app/store"
+import Link from "next/link"
 
 const LoginPage = () => {
     const { setUser } = useAppStore()
     const [dispatchSignin, user, isLoading, error] = useFetch()
     const [{ username, password }, setForm] = useState({
-        username: '',
-        password: '',
+        username: "",
+        password: "",
     })
 
     useEffect(() => {
         if (user) {
             setUser(user)
-            redirect('/')
+            redirect("/")
         }
     }, [user])
 
@@ -24,40 +25,51 @@ const LoginPage = () => {
         dispatchSignin(() => signin({ username, password }))
     }
 
-    const setFormValue = event => {
+    const setFormValue = (event) => {
         event.target.value
-        setForm(state => ({
+        setForm((state) => ({
             ...state,
             [event.target.name]: event.target.value,
         }))
     }
 
     return (
-        <form className='p-10'>
+        <form className="p-10">
             <div>
-                <label>Username</label>
-                <input
-                    name='username'
-                    type='text'
-                    value={username}
-                    onChange={setFormValue}
-                />
+                <label className="mr-2">
+                    <p>Username</p>
+                    <input
+                        name="username"
+                        type="text"
+                        value={username}
+                        onChange={setFormValue}
+                    />
+                </label>
             </div>
-            <div>
-                <label>Password</label>
+            <div className="mt-2">
+                <label className="mr-2">Password</label>
                 <input
-                    name='password'
-                    type='password'
+                    name="password"
+                    type="password"
                     value={password}
                     onChange={setFormValue}
                 />
             </div>
             {isLoading && <div>isLoading</div>}
             <button
-                type='button'
+                type="button"
                 onClick={doLogin}
+                className="bg-white rounded-sm text-black font-bold p-1.5 mt-3"
             >
                 Login
+            </button>
+            <p className="mt-5">No account yet?</p>
+            <button
+                type="button"
+                onClick={doLogin}
+                className="bg-white rounded-sm text-black font-bold p-1.5 mt-1"
+            >
+                <Link href={"/signup"}>Sign Up</Link>
             </button>
         </form>
     )
