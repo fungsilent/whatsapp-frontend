51f5d4e47a15b0d2a697c2cb69cde27c4b87d58b
lpxuan0624 <lipeixuan123@gmail.com>
2024-11-11 22:13:42 +0800

    reze update

--- src/app/(public)/signup/page.js
+++ src/app/(public)/signup/page.js
@@ -1,131 +1,170 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import useFetch from '#root/hooks/useFetch'
-import { signup } from '#root/api/user'
-import Link from 'next/link'
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useFetch from "#root/hooks/useFetch";
+import { signup } from "#root/api/user";
+import Link from "next/link";
+import { useAppStore } from "#root/app/store";
 
 const Signup = () => {
-    const router = useRouter()
+    const { setUser } = useAppStore();
+    const router = useRouter();
     const [form, setForm] = useState({
-        name: '',
-        username: '',
-        password: '',
-        confirmPassword: '',
-    })
-    const [formError, setFormError] = useState('')
-    const [dispatchSignup, data, isLoading, error] = useFetch({
-        log: 'signup',
-    })
+        name: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+    });
+    const [formError, setFormError] = useState("");
+    const [dispatchSignup, user, isLoading, error] = useFetch({
+        log: "signup",
+    });
 
     useEffect(() => {
-        if (data) {
-            router.push('/')
+        if (user) {
+            setUser(user);
+            router.push("/");
         }
-    }, [data])
+    }, [user]);
 
     const doSignup = () => {
         if (form.password !== form.confirmPassword) {
-            return setFormError('Confirm password incorrect')
+            return setFormError("Confirm password incorrect");
         }
-        dispatchSignup(() => signup(form))
-    }
+        dispatchSignup(() => signup(form));
+    };
 
     // 如果撞名彈警告
-    const onFormChange = e => {
-        const name = e.target.name
-        let value = e.target.value
-        if (name === 'confirmPassword') {
-            setFormError('')
+    const onFormChange = (e) => {
+        const name = e.target.name;
+        let value = e.target.value;
+        if (name === "confirmPassword") {
+            setFormError("");
         }
-        if (name === 'username') {
-            value = value.toLowerCase()
+        if (name === "username") {
+            value = value.toLowerCase();
         }
         setForm({
             ...form,
             [name]: value,
-        })
-    }
+        });
+    };
 
     return (
         <>
-            <div className='p-2'>
-                {/* insert icon??? or default icon */}
-                {/* <image
-                    alt=""
-                    className="rounded-full w-14 h-14 aspect-square bg-slate-600"
-                /> */}
-                <label>
-                    <p> Please enter your display name:</p>
-                    <input
-                        name='name'
-                        type='text'
-                        value={form.name}
-                        onChange={onFormChange}
-                        minLength={4}
-                        maxLength={10}
-                        required
-                    ></input>
-                </label>
-                <label>
-                    {/* check username 同 db 有冇已存在/相同 */}
-                    {/* Max 10 characters? */}
-                    <p> Please enter your username:</p>
-                    <input
-                        name='username'
-                        type='text'
-                        value={form.username}
-                        onChange={onFormChange}
-                        minLength={4}
-                        maxLength={10}
-                        required
-                    ></input>
-                </label>
-                <label>
-                    <p>Please enter your password:</p>
-                    <input
-                        name='password'
-                        type='password'
-                        value={form.password}
-                        onChange={onFormChange}
-                    ></input>
-                </label>
-                <label>
-                    <p>Please enter your password:</p>
-                    <input
-                        name='confirmPassword'
-                        type='password'
-                        value={form.confirmPassword}
-                        onChange={onFormChange}
-                    ></input>
-                </label>
-                {/* <label> */}
-                {/* 行個Logic 同上面密碼對係咪一樣 */}
-                {/* <p>Please re-confirm your password:</p> */}
-                {/* <input></input> */}
-                {/* </label> */}
-                <div>
-                    {formError && <p>{formError}</p>}
-                    {error && <p>{error}</p>}
-                </div>
+            <section className="bg-darkblue-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a
+                        href="#"
+                        className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+                    >
+                        <img
+                            className="w-8 h-8 mr-2"
+                            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                            alt="logo"
+                        />
+                        Flowbite
+                    </a>
+                    <div className="w-full bg-gray-900 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+                                Create an account
+                            </h1>
+                            <form className="space-y-4 md:space-y-6">
+                                <div>
+                                    <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-white dark:text-white"
+                                    >
+                                        Your username
+                                    </label>
+                                    <input
+                                        name="username"
+                                        type="text"
+                                        value={form.username}
+                                        onChange={onFormChange}
+                                        minLength={4}
+                                        maxLength={10}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required=""
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-white dark:text-white"
+                                    >
+                                        Your display name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        value={form.name}
+                                        onChange={onFormChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required=""
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-white dark:text-white"
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        name="password"
+                                        type="password"
+                                        value={form.password}
+                                        onChange={onFormChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required=""
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="confirm-password"
+                                        className="block mb-2 text-sm font-medium text-white dark:text-white"
+                                    >
+                                        Confirm password
+                                    </label>
+                                    <input
+                                        name="confirmPassword"
+                                        type="password"
+                                        value={form.confirmPassword}
+                                        onChange={onFormChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required=""
+                                    />
+                                </div>
+                                <div>
+                                    {formError && <p>{formError}</p>}
+                                    {error && <p>{error}</p>}
+                                </div>
 
-                <div className='mt-5'>
-                    <Link
-                        href='/'
-                        className='bg-white rounded-sm text-black font-bold px-2 py-1 mt-1 mr-2'
-                    >
-                        Back
-                    </Link>
-                    <button
-                        onClick={doSignup}
-                        className='bg-white rounded-sm text-black font-bold px-2 py-1mt-1'
-                    >
-                        Sign Up!
-                    </button>
+                                <button
+                                    type="button"
+                                    onClick={doSignup}
+                                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                >
+                                    Create an account
+                                </button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Already have an account?{" "}
+                                    <Link
+                                        href="/login"
+                                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                    >
+                                        Login here
+                                    </Link>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </section>
         </>
-    )
-}
+    );
+};
 
-export default Signup
+export default Signup;
--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,18 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
-export const useAppStore = create(set => ({
+export const useAppStore = create((set) => ({
     user: null,
     isAuth: false,
     socket: null,
-    setUser: user => {
-        const isAuth = !!user
+    setUser: (user) => {
+        const isAuth = !!user;
         set({
             user: isAuth ? user : null,
             isAuth,
-        })
+        });
         if (!isAuth) {
-            localStorage.removeItem('token')
+            localStorage.removeItem("token");
         }
     },
-    setSocket: socket => set({ socket }),
-}))
+    setSocket: (socket) => set({ socket }),
+}));
