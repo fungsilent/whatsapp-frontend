d2539e1b6309d418f9957db27e66fd1813e583df
Terry-Ming <terry030106@gmail.com>
2024-11-18 23:49:07 +0800

    18-11-2024B

--- src/app/(private)/components/chat/panel/AddGroupMember.js
+++ src/app/(private)/components/chat/panel/AddGroupMember.js
@@ -1,121 +1,114 @@
-import { useEffect } from 'react'
-import clsx from 'clsx'
-import { Avatar, Spinner, Popover, Modal } from 'flowbite-react'
-import TextField, { useText } from '#root/components/TextField'
-import Name from '#root/components/Name'
-import useFetch from '#root/hooks/useFetch'
-import { searchUser } from '#root/api/friend'
-import { addMember } from '#root/api/group'
+import { useEffect } from "react";
+import clsx from "clsx";
+import { Avatar, Spinner, Popover, Modal } from "flowbite-react";
+import TextField, { useText } from "#root/components/TextField";
+import Name from "#root/components/Name";
+import useFetch from "#root/hooks/useFetch";
+import { searchUser } from "#root/api/friend";
+import { addMember } from "#root/api/group";
 
 const AddGroupMember = ({ roomId }) => {
-    const [search, setSearch, debounceSearch] = useText('', 300)
-    const [dispatchSearch, users, isLoading, error] = useFetch()
+  const [search, setSearch, debounceSearch] = useText("", 300);
+  const [dispatchSearch, users, isLoading, error] = useFetch();
 
-    useEffect(() => {
-        if (debounceSearch) {
-            dispatchSearch(() => searchUser(search))
-        }
-    }, [debounceSearch])
-
-    const renderList = () => {
-        if (!users) {
-            return null
-        }
-        if (!users.length) {
-            return <p className='py-6 text-sm text-center'>No user found</p>
-        }
-        return users.map((user, index) => (
-            <AddMember
-                key={index}
-                roomId={roomId}
-                {...user}
-            />
-        ))
+  useEffect(() => {
+    if (debounceSearch) {
+      dispatchSearch(() => searchUser(search));
     }
+  }, [debounceSearch]);
 
-    return (
-        <>
-            <div className='p-3 mb-3 bg-white dark:bg-slate-900'>
-                <TextField
-                    placeholder='Search username'
-                    value={search}
-                    onChange={value => setSearch(value)}
-                />
-            </div>
-            <div className='flex-1 bg-white dark:bg-slate-900 overflow-y-auto'>
-                {!search && <p className='py-6 text-sm text-center'>Add member</p>}
-                {search && renderList()}
-            </div>
-        </>
-    )
-}
+  const renderList = () => {
+    if (!users) {
+      return null;
+    }
+    if (!users.length) {
+      return <p className="py-6 text-sm text-center">No user found</p>;
+    }
+    return users.map((user, index) => (
+      <AddMember key={index} roomId={roomId} {...user} />
+    ));
+  };
+
+  return (
+    <>
+      <div className="p-3 mb-3 bg-white dark:bg-slate-900">
+        <TextField
+          placeholder="Search username"
+          value={search}
+          onChange={(value) => setSearch(value)}
+        />
+      </div>
+      <div className="flex-1 bg-white dark:bg-slate-900 overflow-y-auto">
+        {!search && <p className="py-6 text-sm text-center">Add member</p>}
+        {search && renderList()}
+      </div>
+    </>
+  );
+};
 
 const AddMember = ({ roomId, name, username }) => {
-    const [dispatchAdd, isAdd, isLoading, error] = useFetch()
+  const [dispatchAdd, isAdd, isLoading, error] = useFetch();
 
-    const onAdd = () => {
-        dispatchAdd(() =>
-            addMember({
-                roomId,
-                username,
-            })
-        )
-    }
+  const onAdd = () => {
+    dispatchAdd(() =>
+      addMember({
+        roomId,
+        username,
+      })
+    );
+  };
 
-    return (
-        <div
-            className='p-3 border-b-2 border-b-stone-200 dark:border-b-slate-800 hover:bg-slate-100 hover:dark:bg-slate-700 cursor-pointer'
-            onClick={onAdd}
-        >
-            <div className='flex gap-3 items-center text-sm'>
-                <Avatar
-                    rounded
-                    size='md'
+  return (
+    <div
+      className="p-3 border-b-2 border-b-stone-200 dark:border-b-slate-800 hover:bg-slate-100 hover:dark:bg-slate-700 cursor-pointer"
+      onClick={onAdd}
+    >
+      <div className="flex gap-3 items-center text-sm">
+        <Avatar rounded size="md" />
+        <Name type="name">{name}</Name>
+        <Name type="username">{username}</Name>
+        <span className="ml-auto text-xs">
+          {isLoading && <Spinner />}
+          {!isLoading && !error && (
+            <svg
+              className={clsx(
+                "w-6 h-6",
+                { "text-gray-500 dark:text-white": !isAdd },
+                { "text-emerald-600": isAdd }
+              )}
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          )}
+          {error && (
+            <Popover
+              trigger="hover"
+              placement="left"
+              content={<p className="py-1 px-2">{error}</p>}
+            >
+              <svg
+                className="w-6 h-6 text-rose-600"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+                  clipRule="evenodd"
                 />
-                <Name type='name'>{name}</Name>
-                <Name type='username'>{username}</Name>
-                <span className='ml-auto text-xs'>
-                    {isLoading && <Spinner />}
-                    {!isLoading && !error && (
-                        <svg
-                            className={clsx(
-                                'w-6 h-6',
-                                { 'text-gray-500 dark:text-white': !isAdd },
-                                { 'text-emerald-600': isAdd }
-                            )}
-                            fill='currentColor'
-                            viewBox='0 0 24 24'
-                        >
-                            <path
-                                fillRule='evenodd'
-                                d='M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z'
-                                clipRule='evenodd'
-                            />
-                        </svg>
-                    )}
-                    {error && (
-                        <Popover
-                            trigger='hover'
-                            placement='left'
-                            content={<p className='py-1 px-2'>{error}</p>}
-                        >
-                            <svg
-                                className='w-6 h-6 text-rose-600'
-                                fill='currentColor'
-                                viewBox='0 0 24 24'
-                            >
-                                <path
-                                    fillRule='evenodd'
-                                    d='M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z'
-                                    clipRule='evenodd'
-                                />
-                            </svg>
-                        </Popover>
-                    )}
-                </span>
-            </div>
-        </div>
-    )
-}
+              </svg>
+            </Popover>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default AddGroupMember
+export default AddGroupMember;
--- src/app/(private)/components/primary/addfriend.js
+++ src/app/(private)/components/primary/addfriend.js
@@ -2,10 +2,12 @@
 import { addFriend, searchUser } from "#root/api/friend";
 import useFetch from "#root/hooks/useFetch";
 import { useState } from "react";
+import Name from "#root/components/Name";
+import clsx from "clsx";
+import { Avatar, Spinner, Popover, Modal } from "flowbite-react";
 
 function NewFriend() {
   const [userlist, setUserlist] = useState([]);
-  const [dispatchAdd, user, isLoading, error] = useFetch();
   const [newUsername, setNewUsername] = useState("");
 
   const handleChange = (event) => {
@@ -18,11 +20,6 @@ function NewFriend() {
     setUserlist(data.data);
   };
 
-  const add = async (username) => {
-    dispatchAdd(() => addFriend({ username }));
-    console.log(username, " added");
-  };
-
   console.log(userlist);
   return (
     <div>
@@ -32,13 +29,13 @@ function NewFriend() {
           <input
             className="w-75% rounded px-3 py-1 bg-stone-200 dark:bg-slate-600 placeholder-gray-400 outline-none',"
             onChange={handleChange}
+            placeholder="Insert friend username"
           ></input>
-          {isLoading && <div>{isLoading}</div>}
-          {error && <div>{error}</div>}
+
           <button onClick={() => search(newUsername)}>
             {" "}
             <svg
-              className="w-8 h-8 text-gray-800 dark:text-white"
+              className="w-8 h-8 ml-4 text-gray-800 dark:text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -56,20 +53,70 @@ function NewFriend() {
           </button>
         </div>
       </div>
-      <div className="ml-4">
+      <div className="m-4  border-t-2 p-2">
         {userlist.map((user, index) => (
-          <div className="flex flex-col my-4" key={index}>
-            <div>name:{user.name}</div>
-            <div>username:{user.username}</div>
-            <div className="cursor-pointer" onClick={() => add(user.username)}>
-              Add
-            </div>
-          </div>
+          <Addfriend key={index} user={user} />
         ))}
-        <div>{error ? error : ""} </div>
       </div>
     </div>
   );
 }
+
+const Addfriend = ({ user, index }) => {
+  const [dispatchAdd, isAdd, isLoading, error] = useFetch();
+
+  const add = async (username) => {
+    dispatchAdd(() => addFriend({ username }));
+    console.log(username, " added");
+  };
+
+  return (
+    <div className="flex flex-col  border-b-2  	" key={index}>
+      <Name type="name">{user.name}</Name>
+      <Name type="username">{user.username}</Name>
+
+      <span className=" text-xs">
+        {isLoading && <Spinner />}
+        {!isLoading && !error && (
+          <svg
+            onClick={() => add(user.username)}
+            className={clsx(
+              "w-6 h-6 cursor-pointer",
+              { "text-gray-500 dark:text-white": !isAdd },
+              { "text-emerald-600": isAdd }
+            )}
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+        {error && (
+          <Popover
+            trigger="hover"
+            placement="left"
+            content={<p className="py-1 px-2">{error}</p>}
+          >
+            <svg
+              className="w-6 h-6 text-rose-600"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Popover>
+        )}
+      </span>
+    </div>
+  );
+};
 
 export default NewFriend;
