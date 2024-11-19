ac19a95abd9b92140775ced67f7c34fc5e724058
Terry-Ming <terry030106@gmail.com>
2024-11-19 14:24:19 +0800

    Update addfriend.js

--- src/app/(private)/components/primary/addfriend.js
+++ src/app/(private)/components/primary/addfriend.js
@@ -11,23 +11,11 @@ function NewFriend() {
   const [search, setSearch, debounceSearch] = useText("", 300);
   const [dispatchSearch, users, isLoading, error] = useFetch();
 
-  const [userlist, setUserlist] = useState([]);
-  const [newUsername, setNewUsername] = useState("");
-
   useEffect(() => {
     if (debounceSearch) {
       dispatchSearch(() => searchUser(debounceSearch));
     }
   }, [debounceSearch]);
-
-  const handleChange = (value) => {
-    setNewUsername(value);
-  };
-
-  //   const search = async (newUsername) => {
-  //     const data = await searchUser(newUsername);
-  //     setUserlist(data.data);
-  //   };
 
   console.log(users);
   return (
@@ -35,36 +23,11 @@ function NewFriend() {
       <p className="py-2 px-4 text-2xl">Search friend</p>
       <div className="flex content-center m-4  mt-0 border-b-2 pb-4">
         <div className="flex content-center  ">
-          {/* <input
-            className="w-75% rounded px-3 py-1 bg-stone-200 dark:bg-slate-600 placeholder-gray-400 outline-none',"
-            onChange={handleChange}
-            placeholder="Insert friend username"
-          ></input> */}
           <TextField
             placeholder="Insert friend username"
             value={search}
             onChange={(value) => setSearch(value)}
           />
-
-          <button onClick={() => search(newUsername)}>
-            {" "}
-            <svg
-              className="w-8 h-8 ml-4 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeWidth="2"
-                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </button>
         </div>
       </div>
       <div>
@@ -107,7 +70,24 @@ const Addfriend = ({ user, index }) => {
         <div className=" m-auto mr-2">
           <span className=" text-xs">
             {isLoading && <Spinner />}
-
+            {!isLoading && !error && (
+              <svg
+                onClick={() => add(user.username)}
+                className={clsx(
+                  "w-6 h-6 cursor-pointer",
+                  { "text-gray-500 dark:text-white": !isAdd },
+                  { "text-emerald-600": isAdd }
+                )}
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
             {error && (
               <Popover
                 trigger="hover"
